23295b4489b505fa39814a18c8ecf7afa826ece2
Francis Pinheiro <efransys@gmail.com>
2022-02-03 14:54:10 -0300

    Project

---
+++ cmd/pkg/render/render.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/francispinheiro/go-course
+
+go 1.17
